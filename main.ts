6f3c522118bc0d4fd502a160d8faadd29d7442e3
宇佐美隆一 <80155159+chateight@users.noreply.github.com>
2022-08-12 14:16:44 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,8 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("" + (receivedNumber))
+})
+music.playMelody("G A - - - - - - ", 300)
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
