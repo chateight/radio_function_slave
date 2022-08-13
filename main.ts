503b0c1594716f354bf65398a8ad3bd3f355d8ca
宇佐美隆一 <80155159+chateight@users.noreply.github.com>
2022-08-13 15:10:24 +0900

    # change numbers to char string

--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    basic.showString("" + (receivedNumber))
-})
 input.onButtonPressed(Button.A, function () {
     id += 1
     basic.showString("" + (id))
     basic.pause(100)
 })
+radio.onReceivedString(function (receivedString) {
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showString(receivedString)
+})
 let id = 0
 music.playMelody("G A - - - - - - ", 300)
 radio.setGroup(1)
-id = 2
+id = 0
 basic.showString("" + (id))
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
         music.playTone(523, music.beat(BeatFraction.Sixteenth))
-        radio.sendNumber(id)
+        radio.sendString("" + (id))
+        basic.pause(1000)
+        id = 0
+        basic.showString("" + (id))
     }
-    basic.pause(1000)
 })
